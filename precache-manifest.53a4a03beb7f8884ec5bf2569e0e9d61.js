b9800816a77a14f9bc7e9ca8058f722d3235be6a
Michelle Picón <u1202018@unimilitar.edu.co>
2020-06-09 02:48:37 -0500

    Updates

---
+++ precache-manifest.53a4a03beb7f8884ec5bf2569e0e9d61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11b479038e250e68079255a0b0e510a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13f518d1c30d097bd4ab",
+    "url": "/static/css/2.2e1250e2.chunk.css"
+  },
+  {
+    "revision": "3882603e1a614e4969b5",
+    "url": "/static/css/main.8a52a618.chunk.css"
+  },
+  {
+    "revision": "13f518d1c30d097bd4ab",
+    "url": "/static/js/2.f0034ecd.chunk.js"
+  },
+  {
+    "revision": "a4ad355058fa34d4c6acf09b37800d53",
+    "url": "/static/js/2.f0034ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3882603e1a614e4969b5",
+    "url": "/static/js/main.f1f76b95.chunk.js"
+  },
+  {
+    "revision": "01f6b554bf29970ec82b",
+    "url": "/static/js/runtime-main.1ec3ddef.js"
+  },
+  {
+    "revision": "57777f03bf7cf005d1feabaa1b95f4b5",
+    "url": "/static/media/semaforo.57777f03.png"
+  }
+]);
